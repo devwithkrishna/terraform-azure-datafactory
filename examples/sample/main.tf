da3e7cdb1c498332ebd4dd4fca127f768436d57a
githubofkrishnadhas <krishnadhasnk1997@gmail.com>
2025-10-14 21:45:27 +0530

    DEVOPS-362 examples

--- examples/sample/main.tf
+++ examples/sample/main.tf
@@ -2,13 +2,16 @@
 
 
 
-# module "azure" {
-#   source = "../../"   # points to the module root
+module "azure" {
+  source = "../../" # points to the module root
 
-#   resource_group_name     = var.resource_group_name
-#   location                = var.location
-#   environment             = var.environment
-#     application_name        = var.application_name
-#     temporary               = var.temporary
+  resource_group_name             = var.resource_group_name
+  location                        = var.location
+  environment                     = var.environment
+  application_name                = var.application_name
+  temporary                       = var.temporary
+  managed_virtual_network_enabled = var.managed_virtual_network_enabled
+  public_network_enabled          = var.public_network_enabled
+  data_factory_name               = var.data_factory_name
 
-# }
+}
--- examples/sample/outputs.tf
+++ examples/sample/outputs.tf
@@ -1,11 +1,26 @@
-# # Outputs for Terratest to validate
+# Outputs for Terratest to validate
 
-# output "resource_group_name" {
-#   description = "Name of the test resource group"
-#   value       = module.azure.resource_group_name
-# }
+output "resource_group_name" {
+  description = "Name of the test resource group"
+  value       = module.azure.resource_group
+}
 
-# output "resource_group_id" {
-#   description = "ID of the test resource group"
-#   value       = module.azure.resource_group_id
-# }
+output "data_factory_name" {
+  description = "Name of the test data factory"
+  value       = module.azure.data_factory_name
+}
+
+output "public_access_enabled" {
+  description = "Public Access enabled or not"
+  value       = module.azure.public_access_enabled
+}
+
+output "data_factory_id" {
+  description = "Id of Azure Data Factory resource"
+  value       = module.azure.data_factory_id
+}
+
+output "datafactory_tags" {
+  description = "Azure data factory tags"
+  value       = module.azure.datafactory_tags
+}
